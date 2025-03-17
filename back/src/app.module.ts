5112377113123c132477fe3d08c71ca30eb41007
momtez-Allouch <allouchmomtez0@gmail.com>
2025-03-17 23:08:11 +0100

    remove unecessary

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ReservationModule } from './reservation/reservation.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [UserModule, ReservationModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
